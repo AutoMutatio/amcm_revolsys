74f89304b13320ebc09318f7ef281513f9a73d66
Paul Austin <paul.austin@automutatio.com>
2022-07-04 21:49:45 +0100

    UUID to base64

--- revolsys-core/src/main/java/com/revolsys/util/Uuid.java
+++ revolsys-core/src/main/java/com/revolsys/util/Uuid.java
@@ -1,5 +1,6 @@
 package com.revolsys.util;
 
+import java.util.Base64;
 import java.util.UUID;
 
 public class Uuid {
@@ -20,4 +21,21 @@ public class Uuid {
     return new UuidNamespace(5, namespace);
   }
 
+  public static String toBase64(final UUID uuid) {
+    final byte[] bytes = toBytes(uuid);
+    return Base64.getEncoder().encodeToString(bytes);
+  }
+
+  public static byte[] toBytes(final UUID uuid) {
+    final long msb = uuid.getMostSignificantBits();
+    final long lsb = uuid.getLeastSignificantBits();
+    final byte[] buffer = new byte[16];
+    for (int i = 0; i < 8; i++) {
+      buffer[i] = (byte)(msb >>> 8 * (7 - i));
+    }
+    for (int i = 8; i < 16; i++) {
+      buffer[i] = (byte)(lsb >>> 8 * (7 - i));
+    }
+    return buffer;
+  }
 }
