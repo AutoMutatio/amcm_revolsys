7a8faf1ac6b86dd41620d54937b90a03714130d2
Paul Austin <paul.austin@automutatio.com>
2022-08-15 08:03:17 -0700

    http

--- revolsys-core/src/main/java/com/revolsys/http/reactor/ReactorHttpRequestBuilder.java
+++ revolsys-core/src/main/java/com/revolsys/http/reactor/ReactorHttpRequestBuilder.java
@@ -9,19 +9,39 @@ import org.apache.http.NameValuePair;
 
 import com.revolsys.util.UriBuilder;
 
+import io.netty.handler.codec.http.DefaultHttpHeaders;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpMethod;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.http.client.HttpClient.RequestSender;
+import reactor.netty.http.client.HttpClient.ResponseReceiver;
 
 public class ReactorHttpRequestBuilder {
 
   private final HttpClient client = HttpClient.create();
 
+  private final DefaultHttpHeaders headers = new DefaultHttpHeaders();
+
   private final UriBuilder uriBuilder = new UriBuilder();
 
-  private HttpMethod method;
-
   private final Map<String, List<NameValuePair>> parameters = new LinkedHashMap<>();
+
+  public ReactorHttpRequestBuilder addHeader(final CharSequence name, final Object value) {
+    this.headers.add(name, value);
+    return this;
+  }
+
+  public final RequestSender delete() {
+    return request(HttpMethod.DELETE);
+  }
+
+  public final ResponseReceiver<?> get() {
+    return request(HttpMethod.GET);
+  }
+
+  public final ResponseReceiver<?> head() {
+    return request(HttpMethod.HEAD);
+  }
 
   public ReactorHttpRequestBuilder headers(final Consumer<HttpHeaders> action) {
     this.client.headers(action);
@@ -31,6 +51,14 @@ public class ReactorHttpRequestBuilder {
   public ReactorHttpRequestBuilder host(final String host) {
     this.uriBuilder.setHost(host);
     return this;
+  }
+
+  public final ResponseReceiver<?> options() {
+    return request(HttpMethod.OPTIONS);
+  }
+
+  public final RequestSender patch() {
+    return request(HttpMethod.PATCH);
   }
 
   public ReactorHttpRequestBuilder path(final String path) {
@@ -43,8 +71,33 @@ public class ReactorHttpRequestBuilder {
     return this;
   }
 
+  public final RequestSender post() {
+    return request(HttpMethod.POST);
+  }
+
+  public final RequestSender put() {
+    return request(HttpMethod.PUT);
+  }
+
+  public ReactorHttpRequestBuilder removeHeader(final CharSequence name) {
+    this.headers.remove(name);
+    return this;
+  }
+
+  public RequestSender request(final HttpMethod method) {
+    final String uri = uri();
+    return HttpClient.create()
+      .headers(headers -> headers.add(this.headers))
+      .request(method)
+      .uri(uri);
+  }
+
+  public ReactorHttpRequestBuilder setHeader(final CharSequence name, final Object value) {
+    this.headers.set(name, value);
+    return this;
+  }
+
   public String uri() {
     return this.uriBuilder.buildString();
   }
-
 }
