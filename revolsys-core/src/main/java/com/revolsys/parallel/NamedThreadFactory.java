27bdc5e54fb2972b9ee78ee80f6fb8ae80bd7bb2
Paul Austin <paul.austin@automutatio.com>
2023-10-12 09:12:17 -0700

    daemon threads

--- revolsys-core/src/main/java/com/revolsys/parallel/NamedThreadFactory.java
+++ revolsys-core/src/main/java/com/revolsys/parallel/NamedThreadFactory.java
@@ -20,6 +20,8 @@ public class NamedThreadFactory implements ThreadFactory {
 
   private final AtomicInteger threadNumber = new AtomicInteger(1);
 
+  private boolean daemon = false;
+
   public NamedThreadFactory() {
     this(Thread.NORM_PRIORITY, null);
   }
@@ -64,11 +66,16 @@ public class NamedThreadFactory implements ThreadFactory {
     final String threadName = this.threadNamePrefix + this.threadNumber.getAndIncrement();
     final LoggingRunnable loggingRunnable = new LoggingRunnable(runnable);
     final Thread thread = new Thread(this.group, loggingRunnable, threadName, 0);
-    if (thread.isDaemon()) {
-      thread.setDaemon(false);
+    if (thread.isDaemon() != this.daemon) {
+      thread.setDaemon(this.daemon);
     }
     thread.setPriority(this.priority);
     return thread;
+  }
+
+  public NamedThreadFactory setDaemon(final boolean daemon) {
+    this.daemon = daemon;
+    return this;
   }
 
   public NamedThreadFactory setNamePrefix(final String namePrefix) {
