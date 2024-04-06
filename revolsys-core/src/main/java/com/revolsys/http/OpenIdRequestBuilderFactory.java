938fdc8cd9365190293943803fba5f122fab6271
Paul Austin <paul.austin@automutatio.com>
2024-04-06 08:22:30 -0700

    http request builder auth

--- revolsys-core/src/main/java/com/revolsys/http/OpenIdRequestBuilderFactory.java
+++ revolsys-core/src/main/java/com/revolsys/http/OpenIdRequestBuilderFactory.java
@@ -1,11 +1,15 @@
 package com.revolsys.http;
 
 import com.revolsys.net.oauth.OpenIdConnectClient;
+import com.revolsys.net.oauth.OpenIdScope;
 
 public class OpenIdRequestBuilderFactory extends BearerTokenRequestBuilderFactory {
+
+  public OpenIdRequestBuilderFactory(final OpenIdConnectClient client, final OpenIdScope scope) {
+    this(client, scope.getScope());
+  }
 
   public OpenIdRequestBuilderFactory(final OpenIdConnectClient client, final String scope) {
     super(token -> client.tokenClientCredentials(scope));
   }
-
 }
