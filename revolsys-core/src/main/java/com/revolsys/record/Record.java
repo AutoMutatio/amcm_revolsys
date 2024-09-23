d9b50085dca11116c9536302ec2eabb72911f782
Paul Austin <81650378+paulaustin-automutatio@users.noreply.github.com>
2024-09-23 13:48:46 -0700

    x

--- revolsys-core/src/main/java/com/revolsys/record/Record.java
+++ revolsys-core/src/main/java/com/revolsys/record/Record.java
@@ -14,6 +14,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Consumer;
 
 import com.revolsys.collection.json.JsonObject;
 import com.revolsys.collection.json.JsonType;
