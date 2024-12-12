bf9e5395c69ba5b1873024d7a6ce7f54725dc0c7
Paul Austin <81650378+paulaustin-automutatio@users.noreply.github.com>
2024-12-12 09:49:06 -0800

    Callback when insertStatement records are inserted

--- revolsys-core/src/main/java/com/revolsys/record/schema/TableRecordStoreInsertStatement.java
+++ revolsys-core/src/main/java/com/revolsys/record/schema/TableRecordStoreInsertStatement.java
@@ -1,12 +1,19 @@
 package com.revolsys.record.schema;
 
+import java.util.function.Function;
+
+import com.revolsys.collection.iterator.BaseIterable;
 import com.revolsys.record.Record;
 import com.revolsys.record.query.InsertStatement;
 
 public class TableRecordStoreInsertStatement extends InsertStatement {
   private final TableRecordStoreConnection connection;
 
-  public TableRecordStoreInsertStatement(final TableRecordStoreConnection connection) {
+  private final AbstractTableRecordStore recordStore;
+
+  public TableRecordStoreInsertStatement(final AbstractTableRecordStore recordStore,
+    final TableRecordStoreConnection connection) {
+    this.recordStore = recordStore;
     this.connection = connection;
   }
 
@@ -18,5 +25,16 @@ public class TableRecordStoreInsertStatement extends InsertStatement {
   @Override
   public Record executeInsertRecord() {
     return this.connection.transactionCall(super::executeInsertRecord);
+  }
+
+  @Override
+  public <V> V executeInsertRecords(final Function<BaseIterable<Record>, V> action) {
+    return super.executeInsertRecords(records -> {
+      final var mappedRecords = records.map(record -> {
+        this.recordStore.insertStatementRecordAfter(this.connection, record);
+        return record;
+      });
+      return action.apply(mappedRecords);
+    });
   }
 }
