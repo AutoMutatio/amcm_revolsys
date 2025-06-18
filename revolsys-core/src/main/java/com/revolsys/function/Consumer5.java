a4d95e2932e23f04b9827f9a777dadaaa1d0289f
Paul Austin <81650378+paulaustin-automutatio@users.noreply.github.com>
2025-06-17 21:50:31 -0700

    x

---
+++ revolsys-core/src/main/java/com/revolsys/function/Consumer5.java
@@ -0,0 +1,6 @@
+package com.revolsys.function;
+
+@FunctionalInterface
+public interface Consumer5<P1, P2, P3, P4, P5> {
+  void accept(P1 parameter1, P2 parameter2, P3 parameter3, P4 parameter4, P5 parameter5);
+}
